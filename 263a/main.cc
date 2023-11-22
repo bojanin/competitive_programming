09faf46c043372c2ee073fe14120f26baa4258eb
Tommy Bojanin <bojanint@gmail.com>
2023-11-22 15:04:45 -0500

    263a

--- 263a/main.cc
+++ 263a/main.cc
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
-#include <string_view>
 
 std::vector<std::string> SplitStr(const std::string &s, const char delimiter) {
   size_t pos_start = 0, pos_end, delim_len = 1;
@@ -18,26 +17,28 @@ std::vector<std::string> SplitStr(const std::string &s, const char delimiter) {
   res.push_back(s.substr(pos_start));
   return res;
 }
-
+//"0 0 0 0 0"
 int main(int argc, char **argv) {
-  std::string fake_string;
-  std::getline(std::cin, fake_string);
-  const int vecs = std::stoi(fake_string);
 
-  int x = 0, y = 0, z = 0;
-  for (int i = 0; i < vecs; ++i) {
+  int moves = 0;
+  for (int i = 0; i < 5; ++i) {
     std::string row;
     std::getline(std::cin, row);
-    const auto &elems = SplitStr(row, ' ');
-    x += std::stoi(elems[0]);
-    y += std::stoi(elems[1]);
-    z += std::stoi(elems[2]);
-  }
 
-  if (x == 0 && y == 0 && z == 0) {
-    printf("YES\n");
-  } else {
-    printf("NO\n");
+    int column = 0;
+    for (const char &c : row) {
+
+      if (c == '1') {
+        moves = std::abs(column - 2) + std::abs(i - 2);
+        goto finish;
+      }
+      if (c == '1' || c == '0') {
+        column += 1;
+      }
+    }
   }
+finish:
+  printf("%d\n", moves);
+
   return 0;
 }
