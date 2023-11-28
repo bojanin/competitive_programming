95a85953cf0e2b4841953f0775383bb04d9325a2
Tommy Bojanin <bojanint@gmail.com>
2023-11-27 19:50:43 -0500

    templace improv, 155a,258a

--- 155a.cc
+++ 155a.cc
@@ -4,7 +4,6 @@
 #include <istream>
 #include <sstream>
 #include <stdio.h>
-#include <vector>
 
 template <typename... T> void Read(T &...args) { ((std::cin >> args), ...); }
 template <typename... T> void Write(T &&...args) {
@@ -17,5 +16,31 @@ int main(int argc, char **argv) {
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
   std::cout.tie(0);
+  int n;
+  Read(n);
+
+  int perfects = 0;
+  int min = 0;
+  Read(min);
+  int max = min;
+  n--;
+  // deb(n);
+  // deb(max);
+  // deb(min);
+
+  while (n > 0) {
+    int m;
+    Read(m);
+    if (m > max) {
+      max = m;
+      perfects++;
+    } else if (m < min) {
+      min = m;
+      perfects++;
+    }
+    n--;
+  }
+  printf("%i\n", perfects);
+
   return 0;
 }
--- 248a.cc
+++ 248a.cc
@@ -4,7 +4,6 @@
 #include <istream>
 #include <sstream>
 #include <stdio.h>
-#include <vector>
 
 template <typename... T> void Read(T &...args) { ((std::cin >> args), ...); }
 template <typename... T> void Write(T &&...args) {
@@ -13,9 +12,43 @@ template <typename... T> void Write(T &&...args) {
 #define deb(x) std::cout << #x << " " << x << std::endl;
 
 int main(int argc, char **argv) {
-  // MEANS YOU CANT USE scanf/printf
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
   std::cout.tie(0);
+  int n;
+  Read(n);
+  int lc = 0;
+  int rc = 0;
+
+  int total = n;
+
+  while (total > 0) {
+    int l, r;
+    Read(l, r);
+    lc += l;
+    rc += r;
+    total--;
+  }
+  int res = 0;
+
+  // case where the
+  // deb(lc); // 2
+  // deb(rc); // 4
+  // deb(n);  // 5
+
+  if (n - lc > n / 2) {
+    res += lc;
+  } else {
+    res += n - lc;
+  }
+
+  if (n - rc > n / 2) {
+    res += rc;
+  } else {
+    res += n - rc;
+  }
+
+  printf("%i\n", res);
+
   return 0;
 }
