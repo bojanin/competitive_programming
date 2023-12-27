fee6497ef9ec3b9859c5cbc7feed7be7f46f4c9c
Tommy Bojanin <bojanint@gmail.com>
2023-12-26 20:26:15 -0500

    450a

--- 450a.cc
+++ 450a.cc
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cctype>
+#include <cmath>
 #include <iostream>
 #include <istream>
 #include <sstream>
@@ -14,42 +15,38 @@
 #define SCMP(s1, s2) strcasecmp(s1, s2)
 #include <strings.h>
 #endif
-#if 0
-#endif
 
 template <typename... T> void Read(T &...args) { ((std::cin >> args), ...); }
 template <typename... T> void Write(T &&...args) {
   ((std::cout << args << " "), ...);
 }
 #define deb(x) std::cout << #x << " " << x << std::endl;
-int arr[100'001];
 
 int main(int argc, char **argv) {
   // MEANS YOU CANT USE scanf/printf
-  std::ios::sync_with_stdio(0);
-  std::cin.tie(0);
-  std::cout.tie(0);
-  // n:idx
-  int n;
-  Read(n);
-  int x;
-  for (int i = 0; i < n; i++) {
+  // std::ios::sync_with_stdio(0);
+  // std::cin.tie(0);
+  // std::cout.tie(0);
+  int n, m;
+  Read(n, m);
+  int max = 0;
+  int max_i = n;
+  int i = 1;
+  while (n > 0) {
+    int x;
     Read(x);
-    arr[x] = i + 1;
+    int ceil = std::ceil((double)x / (double)m);
+    if (x > m && ceil >= max) {
+      max_i = i;
+      max = ceil;
+      // Write("==\n");
+      // deb(max);
+      // deb(max_i);
+    }
+    i++;
+    n--;
   }
-
-  int lookups;
-  Read(lookups);
-
-  int curr;
-  uint64_t v = 0, p = 0;
-  while (lookups > 0) {
-    Read(curr);
-    v += arr[curr];
-    p += n - arr[curr] + 1;
-    lookups--;
-  }
-  printf("%llu %llu\n", v, p);
+  printf("%i\n", max_i);
 
   return 0;
 }
